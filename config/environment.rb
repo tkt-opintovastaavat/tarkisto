2ddb31329ef7dfe5d1f5bb48c8fefd1f5353284a
Sami Saada <sami.saada@gmail.com>
2010-05-31 12:07:42 +0300

    Added version requirment for compass.

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
-  config.gem "compass"
+  config.gem "compass", :version => ">=0.10.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
