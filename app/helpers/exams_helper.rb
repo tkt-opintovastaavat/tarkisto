61bd9e7d37f294737584f1397eb143b056568017
Sami Saada <sami.saada@gmail.com>
2010-06-16 01:04:56 +0300

    Added hierarchy for locales. Added Finnish locale for rails file.

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -1,16 +1,2 @@
 module ExamsHelper
-	
-	# Tab titles and their corresponding locales
-	def all_exams
-		t :all_exams
-	end
-	
-	def generate_exam
-		t :generate_exam
-	end
-	
-	def add_exam
-		t :add_exam
-	end	
-	
 end
