f391dae58641a10b55d0a3535db7fa9dec8137f0
Anna Kuosmanen <anna.kuosmanen@cs.helsinki.fi>
2010-06-23 11:44:09 -0400

    Added named_scopes.

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,4 +3,8 @@ class Exam < ActiveRecord::Base
      belongs_to :exam_type
 
      validates_presence_of :date, :lecturer, :maximum_points
+     
+     named_scope :repeat_exams, :conditions => "(exam_type_id = 3)"
+     named_scope :course_exams, :conditions => "(exam_type_id = 2)"
+     named_scope :separate_exams, :conditions => "(exam_type_id = 1)"
 end
