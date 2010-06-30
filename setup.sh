a3d3290633a2a4bbd52e1feedaff121c493d5416
Petri T Karjalainen <ptkarjal@svm-16>
2010-06-30 14:31:39 +0300

    Made database query script for generating seed file. Broke exam finding from view. NEED FIXING

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 echo "Copying database"
 cp config/database.yml.example config/database.yml >> /dev/null
 echo "Setting up database"
-rake db:drop >> /dev/null
-rake db:drop RAILS_ENV=test >> /dev/null
-rake db:migrate >> /dev/null
-rake db:setup >> /dev/null
-rake db:setup RAILS_ENV=test >> /dev/null
+rake db:drop # >> /dev/null
+rake db:drop RAILS_ENV=test # >> /dev/null
+rake db:migrate # >> /dev/null
+rake db:setup # >> /dev/null
+rake db:setup RAILS_ENV=test # >> /dev/null
 echo "Done!"
