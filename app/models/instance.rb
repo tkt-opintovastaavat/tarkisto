47c6dbf8f24e0aa64fbba0ea11ed78fe6703e27d
Anni Laurila <anni.laurila@cs.helsinki.fi>
2010-06-30 10:41:58 +0300

    added following databasetables -attendance -lecturer

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -1,5 +1,6 @@
 class Instance < ActiveRecord::Base
      belongs_to :course
+     has_many :attendances
      
      # periodi olis hyvä olla pakollinen, mutta kun se ei ole "kurki-kannassakaan"...
      validates_presence_of :year, :semester
