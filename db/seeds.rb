0c0d939f5127990f52f48dc74355cc2fc5cd9372
Petri T Karjalainen <ptkarjal@svm-16>
2010-07-01 11:35:49 +0300

    Fixed tests showing at view and modified database query script to work properly.

--- db/seeds.rb
+++ db/seeds.rb
@@ -380,20 +380,12 @@ aine.courses.create :code => 582336, :name_fi => "Introduction to Go", :name_se 
      kurssikoe = Type.create :name_fi => 'Kurssikoe'
      uusintakoe = Type.create :name_fi => 'Uusintakoe'     
 
-     ohpe = perusopinnot.courses.create :code => 581325 , :name_fi => 'Ohjelmoinnin perusteet', :credits => 5
-     lama = aineopinnot.courses.create :code => 582206 , :name_fi => 'Laskennan mallit', :credits => 6
-     bipe = syventävätopinnot.courses.create :code => 582313 , :name_fi => 'Bioinformatiikan perusteet', :credits => 4
-     dima = muutopinnot.courses.create :code => 57049 , :name_fi => 'Johdatus diskreettiin matematiikkaan', :credits => 5
+     Course.find_by_code(581325).exams.create :lecturer => 'Arto Wikla', :type_id => kurssikoe.id, :public => true, :date => Date.new(2010, 6, 12), :maximum_points => 50
+     Course.find_by_code(581325).exams.create :lecturer => 'Luukkainen', :type_id => erilliskoe.id, :date => Date.new(2010, 6, 1), :maximum_points => 60
+     Course.find_by_code(581325).exams.create :lecturer => 'Arto Wikla', :type_id => uusintakoe.id, :public => true, :date => Date.new(2010, 8, 30), :maximum_points => 50
 
-     ohpe.exams.create :lecturer => 'Arto Wikla', :type_id => kurssikoe.id, :public => true, :date => Date.new(2010, 6, 12), :maximum_points => 50
-     ohpe.exams.create :lecturer => 'Luukkainen', :type_id => erilliskoe.id, :date => Date.new(2010, 6, 1), :maximum_points => 60
-     ohpe.exams.create :lecturer => 'Arto Wikla', :type_id => uusintakoe.id, :public => true, :date => Date.new(2010, 8, 30), :maximum_points => 50
+     Course.find_by_code(582206).exams.create :lecturer => 'Kärkkäinen', :type_id => erilliskoe.id , :date => Date.new(2010, 6, 4), :maximum_points => 60
+     Course.find_by_code(582206).exams.create :lecturer => 'Kärkkäinen', :type_id => kurssikoe.id , :date => Date.new(2009, 10 , 22), :maximum_points => 60
+     Course.find_by_code(582206).exams.create :lecturer => 'Kärkkäinen', :type_id => uusintakoe.id , :date => Date.new(2010, 1, 22), :maximum_points => 60
 
-     lama.exams.create :lecturer => 'Kärkkäinen', :type_id => erilliskoe.id , :date => Date.new(2010, 6, 4), :maximum_points => 60
-     lama.exams.create :lecturer => 'Kärkkäinen', :type_id => kurssikoe.id , :date => Date.new(2009, 10 , 22), :maximum_points => 60
-     lama.exams.create :lecturer => 'Kärkkäinen', :type_id => uusintakoe.id , :date => Date.new(2010, 1, 22), :maximum_points => 60
-
-     bipe.exams.create :lecturer => 'Sirkka-Liisa Varvio', :type_id => kurssikoe.id , :date => Date.new(2009, 10, 21), :maximum_points => 60
-
-     dima.exams.create :lecturer => 'Petteri Harjulehto', :type_id => kurssikoe.id , :date => Date.new(2009, 12, 18), :maximum_points => 24
-     dima.exams.create :lecturer => 'Petteri Harjulehto', :type_id => erilliskoe.id , :date => Date.new(2010, 8, 12), :maximum_points => 24
+     Course.find_by_code(582313).exams.create :lecturer => 'Sirkka-Liisa Varvio', :type_id => kurssikoe.id , :date => Date.new(2009, 10, 21), :maximum_points => 60
