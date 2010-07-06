64bd3feb6c4c89e5b13cea1fd94de5f540c0a580
Anni Laurila <anni.laurila@cs.helsinki.fi>
2010-07-06 15:54:46 +0300

    Added autocomplete search partly.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,5 +10,19 @@ class SearchController < ApplicationController
           # raise courses.first["key"].inspect
           @result = courses.select {|value| value["key"] == @q }
      end
+     
+     # siirrä tämä kurssi-kontrolleriin
+     def courses
+          @courses_json = format_json(Course.all)
+          respond_to do |format|
+               format.json{render :json => @courses}
+          end
+     end
+     
+     # siirrä tämä kurssi-kontrolleriin
+     protected
+     def format_json courses
+          courses.collect{|course| {:id => course.id, :value => course.name, :label => course.name}}
+     end
 
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -19,6 +19,13 @@ describe SearchController do
                get 'show', :query => "lama"
                assigns(:result).should == [{"key" => "lama", "name" => "Laskennan mallit", "course_id" => 1}]
           end
+          #siirrä tämäkin course_controller_speciin
+          it "should search all courses" do
+               @course_mock = mock_model Course
+               @course_mock.should_receive(:name).twice.and_return("fu-")
+               Course.should_receive(:all).and_return([@course_mock])
+               get 'courses', :format => :json
+          end
      end
 
 end
