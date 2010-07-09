1b82c3f74ec2f3b35d4c651bf675270bef620a59
Sami Saada <sami.saada@gmail.com>
2010-07-09 21:03:09 +0300

    Fixed tests to pass and cover all.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
 describe Course do
+
      before(:each) do
           @valid_attributes = {
-               :code => 12345 ,
-               :name_fi => 'Ohjelmoinnin perusteet' ,
-               :name_se => 'Introduktion till programmering' ,
-               :name_en => 'Introduction to Programming' ,
-               :level_id => 1 ,
-               :credits => 5 ,
+               :code => 12345,
+               :name_fi => 'Ohjelmoinnin perusteet',
+               :name_se => 'Introduktion till programmering',
+               :name_en => 'Introduction to Programming',
+               :level_id => 1,
+               :credits => 5,
                :replaced_by => 2
           }
           @invalid_course = Course.new
@@ -19,11 +20,11 @@ describe Course do
      end
 
      it "should have a course code" do
-          @invalid_course.should have(1).error_on(:code)          
+          @invalid_course.should have(1).error_on(:code)
      end
 
      it "should have a name" do
-          @invalid_course.should have(1).error_on(:name_fi)          
+          @invalid_course.should have(1).error_on(:name_fi)
      end
 
      it "should have credits" do
@@ -51,13 +52,34 @@ describe Course do
           end
 
      end
-     
+
+     describe "#has_exams" do
+
+          before :each do
+               @exam_mock = mock_model Exam
+               @exams_mock = [@exam_mock]
+               @course = Course.new
+               @course.should_receive(:exams).and_return(@exams_mock)
+          end
+
+          it "should tell if Course has exams" do
+               @exams_mock.should_receive(:published).and_return([{}])
+               @course.has_exams?.should == true
+          end
+
+          it "should tell if Course doesn't have any exams" do
+               @exams_mock.should_receive(:published).and_return([])
+               @course.has_exams?.should == false
+          end
+
+     end
+
      describe "#search" do
-          
-          before :each do 
+
+          before :each do
                Course.create! @valid_attributes
           end
-          
+
           it "should search by full name in finnish" do
                results = Course.search "Ohjelmoinnin perusteet"
                found = false
@@ -67,16 +89,16 @@ describe Course do
                found.should == true
 
           end
-          
+
           it "should search by partial" do
                results = Course.search "moin"
                found = false
                results.each do |result|
-                    found = true unless result.name.scan(/moin/).empty? 
+                    found = true unless result.name.scan(/moin/).empty?
                end
                found.should == true
           end
-          
+
           it "should search name in English" do
                I18n.stub!(:locale).and_return(:en)
                results = Course.search "Introduction to Programming"
@@ -94,63 +116,64 @@ describe Course do
                     found == true if result.name == "Introduktion till programmering"
                end
           end
-          
+
           it "should return nil if locale fails" do
                I18n.stub!(:locale).and_return(nil)
                results = Course.search "Ohjelmoinnin perusteet"
                results.should == nil
           end
-          
-          it "should sort using sorting_priority sit1" do
-               course1 = mock_model Course
-               course2 = mock_model Course
-               course1_name = "course1"
-               course2_name = "course2"
-               course1.stub!(:name).and_return course1_name
-               course2.stub!(:name).and_return course2_name
-               course1_name.should_receive(:scan).and_return [{}]
-               course2_name.should_receive(:scan).and_return [{}]
-               situation = Course.sorting_priority "cour", course1, course2             
-               situation.should == -1
+
+          describe "#sorting_priority" do
+
+               before :each do
+                    @course1 = Course.new :name_fi => "course1"
+                    @course2 = Course.new :name_fi => "course2"
+               end
+
+               it "should sort using sorting_priority sit1" do
+                    @course1.name.should_receive(:scan).and_return [{}]
+                    @course2.name.should_receive(:scan).and_return [{}]
+                    situation = Course.sorting_priority "course", @course1, @course2
+                    situation.should == -1
+               end
+
+               it "should sort using sorting_priority sit2" do
+                    @course1.name.should_receive(:scan).and_return [{}]
+                    @course2.name.should_receive(:scan).and_return [{}]
+                    situation = Course.sorting_priority "course", @course2, @course1
+                    situation.should == 1
+               end
+
+               it "should sort using sorting_priority sit3" do
+                    @course1.name.should_receive(:scan).and_return [{}]
+                    @course2.name.should_receive(:scan).and_return []
+                    situation = Course.sorting_priority "course", @course1, @course2
+                    situation.should == -1
+               end
+
+               it "should sort using sorting_priority sit4" do
+                    @course1.name.should_receive(:scan).and_return []
+                    @course2.name.should_receive(:scan).and_return [{}]
+                    situation = Course.sorting_priority "course", @course1, @course2
+                    situation.should == 1
+               end
+
+               it "should sort using sorting_priority sit5" do
+                    @course1.name.should_receive(:scan).and_return []
+                    @course2.name.should_receive(:scan).and_return []
+                    situation = Course.sorting_priority "course", @course1, @course2
+                    situation.should == -1
+               end
+
+               it "should sort using sorting_priority sit6" do
+                    @course1.name.should_receive(:scan).and_return []
+                    @course2.name.should_receive(:scan).and_return []
+                    situation = Course.sorting_priority "course", @course2, @course1
+                    situation.should == 1
+               end
+
           end
 
-          it "should sort using sorting_priority sit2" do
-               course1 = mock_model Course
-               course2 = mock_model Course
-               course1_name = "course1"
-               course2_name = "ohpe"
-               course1.stub!(:name).and_return course1_name
-               course2.stub!(:name).and_return course2_name
-               course1_name.should_receive(:scan).and_return [{}]
-               course2_name.should_receive(:scan).and_return [{}]
-               situation = Course.sorting_priority "cour", course1, course2             
-               situation.should == -1
-          end
-            
-          it "should sort using sorting_priority sit3" do
-               course1 = mock_model Course
-               course2 = mock_model Course
-               course1_name = "ohpe"
-               course2_name = "course2"
-               course1.stub!(:name).and_return course1_name
-               course2.stub!(:name).and_return course2_name
-               course1_name.should_receive(:scan).and_return [{}]
-               course2_name.should_receive(:scan).and_return [{}]
-               situation = Course.sorting_priority "cour", course1, course2             
-               situation.should == 1
-          end
-               
-          it "should sort using sorting_priority sit4" do
-               course1 = mock_model Course
-               course2 = mock_model Course
-               course1_name = "course1"
-               course2_name = "course2"
-               course1.stub!(:name).and_return course1_name
-               course2.stub!(:name).and_return course2_name
-               course1_name.should_receive(:scan).and_return [{}]
-               course2_name.should_receive(:scan).and_return [{}]
-               situation = Course.sorting_priority "ohpe", course1, course2             
-               situation.should == -1     
-          end
      end
+
 end
