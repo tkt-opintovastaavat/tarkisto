6c9d4b553dab5dedfecd08c60fbd76d3b604deea
Petri T Karjalainen <ptkarjal@svm-16>
2010-07-30 10:45:44 +0300

    Modified file saving path to public/question_images/:id as well as changed the name of the attachment to more describing (picture -> question_image)

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
      belongs_to :question
-     has_attached_file :picture, :styles => { :medium => "600x600", :thumb => "100x100" }
+     has_attached_file :question_image, :styles => { :medium => "600x600", :thumb => "100x100" }, :path => ":rails_root/public/:attachment/:id/:style_:basename.:extension"
      
-     validates_attachment_presence :picture
-     validates_attachment_content_type :picture, :content_type => /image/
+     validates_attachment_presence :question_image
+     validates_attachment_content_type :question_image, :content_type => /image/
 end
--- db/migrate/20100729092917_add_imagefile_to_image.rb
+++ db/migrate/20100729092917_add_imagefile_to_image.rb
@@ -1,15 +1,15 @@
 class AddImagefileToImage < ActiveRecord::Migration
   def self.up
-     add_column :images, :picture_file_name,    :string
-     add_column :images, :picture_content_type, :string
-     add_column :images, :picture_file_size,    :integer
+     add_column :images, :question_image_file_name,    :string
+     add_column :images, :question_image_content_type, :string
+     add_column :images, :question_image_file_size,    :integer
      remove_column :images, :image_path
   end
 
   def self.down
-     remove_column :images, :picture_file_name,    :string
-     remove_column :images, :picture_content_type, :string
-     remove_column :images, :picture_file_size,    :integer
+     remove_column :images, :question_image_file_name,    :string
+     remove_column :images, :question_image_content_type, :string
+     remove_column :images, :question_image_file_size,    :integer
      add_column :images, :image_path
   end
 end
