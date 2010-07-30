006bf202ac9a03d24adb51f3d3842daadd9c49a8
Petri T Karjalainen <ptkarjal@svm-16>
2010-07-31 02:57:51 +0300

    Added support from reading codes and images from description.

--- lib/pdf_export.rb
+++ lib/pdf_export.rb
@@ -13,14 +13,39 @@ module PdfExport
      def self.question_format(exam, pdf)
           exam.questions.each do |question|
                pdf.text("\n#{question.number}. #{question.name}", :font_size => 10)
-               pdf.text("\n#{question.description}", :left => 30, :font_size => 8)
+               filter = /<\[[ci]#[0-9]+\]>/
+               attach = /<\[([ci])#([0-9]+)\]>/
+               texts = question.description.split(filter)
+               objects = question.description.scan(filter).map do |object|
+                    object = object.split(attach)
+                    if object[1] == 'i'
+                         question.images[object[2].to_i-1]
+                    else
+                         question.code_snippets[object[2].to_i-1]
+                    end
+               end
 
-               question.code_snippets.each do |code|
-                    pdf.text code.text
+               texts << "" if texts.empty?
+
+               texts.each do |str|
+                    pdf.text str, :left => 30, :font_size => 8
+                    unless objects.empty?
+                         object = objects.shift
+                         if object.instance_of? Image
+                              pdf.image object.question_image.url( :pdf )
+                         else
+                              pdf.text object.text
+                         end
+                    end
                end
-               question.images.each do |image|
-                    pdf.image image.question_image.url( :pdf )
-               end
+               #pdf.text("\n#{question.description}", :left => 30, :font_size => 8)
+
+               #question.code_snippets.each do |code|
+               #     pdf.text code.text
+               #end
+               #question.images.each do |image|
+               #     pdf.image image.question_image.url( :pdf )
+               #end
 
                pdf.text("(#{question.points} #{I18n.t'pdf.exam.points'})", :justification => :center, :font_size => 8)
 
