e8b93468681a0af82b995df38ddfe3a55d7e3901
Tuomo Niemelä <tuomo.a.niemela@gmail.com>
2010-07-30 15:39:23 +0300

    fixed dashboard. 1 spec fails.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,22 @@
 class DashboardController < ApplicationController
-
+     
      def show
-          @basic_studies = Course.basic_courses.alphabetize.courses_on_semester 2010, 'S'
-          @intermediate_studies = Course.intermediate_courses.alphabetize.courses_on_semester 2010, 'S'
-          @advanced_studies = Course.advanced_courses.alphabetize.courses_on_semester 2010, 'S'
-          @other_studies = Course.other_courses.alphabetize.courses_on_semester 2010, 'S'
+          @basic_studies = Level.find_by_name_fi("Perusopinnot").courses.alphabetize.courses_on_semester Date.today.year, semester
+          @intermediate_studies = Level.find_by_name_fi("Aineopinnot").courses.alphabetize.courses_on_semester Date.today.year, semester
+          @advanced_studies = Level.find_by_name_fi("Syventävät opinnot").courses.alphabetize.courses_on_semester Date.today.year, semester
+          @other_studies = Level.find_by_name_fi("Muut opinnot").courses.alphabetize.courses_on_semester Date.today.year, semester
+     end
+     
+     protected
+     def semester
+          @month = Date.today.month
+          if (1..5).include?(@month)
+               'K'
+          elsif (6..8).include?(@month)
+               'S'# <---DEMO!! LAITA TÄHÄN: 'V'
+          else (9..12).include?(@month)
+               'S'
+          end
      end
 
 end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -5,6 +5,7 @@ describe DashboardController do
      before :each do
           @level_mock = mock_model Level
           @course_mock = mock_model Course
+          @courses_mock = [@course_mock]
      end
 
      it "should use DashboardController" do
@@ -17,9 +18,10 @@ describe DashboardController do
                Level.should_receive(:find_by_name_fi).with("Aineopinnot").and_return(@level_mock)
                Level.should_receive(:find_by_name_fi).with("Muut opinnot").and_return(@level_mock)
                Level.should_receive(:find_by_name_fi).with("Syventävät opinnot").and_return(@level_mock)
-               4.times do
-                    @level_mock.should_receive(:courses).and_return([@course_mock])
+               4.times do                   
+                    @level_mock.should_receive(:courses).and_return([@course_mock]) 
                end
+
                get 'show'
                assigns(:basic_studies).should==[@course_mock]
                assigns(:intermediate_studies).should==[@course_mock]
