7c162b691f4b464b3bde304d1ca6243145e5d412
Petri T Karjalainen <ptkarjal@svm-16>
2010-07-30 15:02:41 +0300

    Added windows support for filetype uploading.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,5 +3,5 @@ class Image < ActiveRecord::Base
      has_attached_file :question_image, :styles => { :web => "600x600>"}, :path => ":rails_root/public/:attachment/:id/:style_:basename.:extension", :url => ":rails_root/public/:attachment/:id/:style_:basename.:extension"
      
      validates_attachment_presence :question_image
-     validates_attachment_content_type :question_image, :content_type => /image/
+     validates_attachment_content_type :question_image, :content_type => ["image/jpeg", "image/pjpeg", "image/gif", "image/png", "image/x-png", "image/jpg"]
 end
