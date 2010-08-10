77864cf05cac25d906c694b8833ee220027e6a3c
Petri T Karjalainen <ptkarjal@svm-16>
2010-08-10 14:14:21 +0300

    Added JSON object formatting for questions.

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -16,4 +16,32 @@ module ExamsHelper
           end
      end
 
+     def generate_data_object exam
+          unless exam[:id].nil?
+               questions = []
+               exam.questions.each do |q|
+                    _question_to_json(q, questions)
+               end
+          end
+     end
+
+     private
+
+     def _question_to_json q, questions
+          images = []
+          q.images.each do |i|
+               images << i.attributes.to_json
+          end
+          codes = []
+          q.code_snippets.each do |c|
+               codes << c.attributes.to_json
+          end
+          themes = []
+          q.themes.each do |t|
+               themes << t.attributes.to_json
+          end
+          questions << q.attributes.merge(
+               {"images" => images, "codes" => codes, "themes" => themes}
+          ).to_json
+     end
 end
