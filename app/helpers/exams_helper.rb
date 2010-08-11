b4e10b21e8ec8adda047bd9e99cab544638a9771
Petri T Karjalainen <ptkarjal@svm-16>
2010-08-11 13:53:33 +0300

    Added support for saving data_object into database.

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -23,28 +23,88 @@ module ExamsHelper
                    questions << _question_to_json(q)
                end
           end
-          data_object = {"exam" => exam, "questions" => questions, "modified" => false}
+          examhash = exam.attributes
+
+          data_object = {"exam" => examhash, "questions" => questions, "modified" => false}
           return data_object.to_json
-          
+     end
+
+     def save_data_object object
+
+          object = JSON::parse object
+
+          unless object["exam"]["id"].nil?
+               exam = Exam.find_by_id object["exam"]["id"]
+          else
+               exam = _new_exam object["exam"]
+          end
+
+          object["questions"].each do |q|
+               unless q["id"].nil?
+                    question = Question.find_by_id q["id"]
+               else
+                    question = _new_question q, exam
+               end
+               q["codes"].each do |c|
+                    if question.code_snippets.find_by_id(c).nil?
+                         code = CodeSnippet.find_by_id c
+                         code.question_id = question
+                         code.save
+                    end
+               end
+               q["images"].each do |i|
+                    if question.images.find_by_id(i).nil?
+                         image = Image.find_by_id i
+                         image.question_id = question
+                         image.save
+                    end
+               end
+               q["course_themes"].each do |t|
+                    if question.course_themes.find_by_id(t).nil?
+                         course_theme = CourseTheme.find_by_id t
+                         question.course_theme_questions.create :course_theme => course_theme
+                    end
+               end
+          end
      end
 
      private
 
+     def _new_exam e
+          course = Course.find_by_id e["course_id"]
+          course.exams.only_public.create( 
+               :lecturer => 0 ,
+               :type_id => e["type_id"], 
+               :maximum_points => e["maximum_points"], 
+               :date => e["date"], 
+               :language => e["language"]
+          )
+     end
+
+     def _new_question q, e
+          e.questions.create( 
+               :number => q["number"],
+               :description => q["description"],
+               :name => q["name"],
+               :points => q["points"]
+          ) 
+     end
+
      def _question_to_json q
           images = []
           q.images.each do |i|
-               images << i.attributes
+               images << i.attributes["id"]
           end
           codes = []
           q.code_snippets.each do |c|
-               codes << c.attributes
+               codes << c.attributes["id"]
           end
-          themes = []
+          course_themes = []
           q.themes.each do |t|
-               themes << t.attributes
+               course_themes << t.attributes["id"]
           end
           return q.attributes.merge(
-               {"images" => images, "codes" => codes, "themes" => themes}
+               {"images" => images, "codes" => codes, "course_themes" => course_themes}
           )
      end
 end
