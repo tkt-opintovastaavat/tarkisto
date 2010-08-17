b609f3cdf96e8f95db07daed0d7de27041d5f546
Anna Kuosmanen <anna.kuosmanen@cs.helsinki.fi>
2010-08-17 01:12:47 -0400

    Added changing single questions. It's a checkbox variety for the ease of coding this proto, if customer chooses this version, it will be changed into buttons, and the whole page will be made overall better looking.

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -92,6 +92,7 @@ class ExamsController < ApplicationController
                @generated_exam.course_id = @course.id
                @generated_exam.date = Date.today
                @generated_exam.type_id = Type.find_by_name_fi("Generoitu koe").id
+               @generated_exam.maximum_points = 0
                          
                # Get all the questions for the course in random order, then pick only ones that match the themes
                # and add those to the exam. Stop when have as many questions as the user wanted.
@@ -121,23 +122,84 @@ class ExamsController < ApplicationController
                # Fix the question numbers
                @generated_exam.questions.each_with_index do |question, index|
                     question.number = index+1
+                    @generated_exam.maximum_points = @generated_exam.maximum_points + question.points
                end
 
-               
-               @questions_string = ""
+               # Make the hidden id string passing the question ids
+               @questions_string = ''
                
                @generated_exam.questions.each do |question|
                     if question.number == 1
                          @questions_string = @questions_string + question.id.to_s
                     else
-                         @question_string = @question_string + "," + question.id.to_s
+                         @questions_string = @questions_string + ',' + question.id.to_s
                     end
                end
                
                
+          elsif params[:commit] == t('pages.exams.forms.generate.generate')
+
+               # Generate the basic info for the exam, again, because objects don't pass correctly in params
+               @generated_exam = Exam.new
+               @generated_exam.course_id = @course.id
+               @generated_exam.date = Date.today
+               @generated_exam.type_id = Type.find_by_name_fi("Generoitu koe").id
+               @generated_exam.maximum_points = 0
                
+               # Parse the question ids from params
+               @question_ids = params['questions'].split(',')
                
+               # Compare the numbers of the questions to be changed to ids of all the questions to filter out the ones to keep
+               @question_ids.each_with_index do |id, index|
+                    if params[(index+1).to_s] == 'chosen'
+                         # It's chosen to be changed, so don't take it
+                    else
+                         @generated_exam.questions << Question.find_by_id(id)
+                    end
                
+               end
+               
+               # Get all the questions for the course in random order, then pick only ones that match the themes
+               # and add those to the exam. Stop when have as many questions as the user wanted.
+               @all_questions = Course.find_by_id(@course).questions
+               @all_questions = @all_questions.sort_by{rand}
+                         
+               @all_questions.each do |question|
+                         
+                    if @generated_exam.questions.size == @question_ids.size
+                         break
+                    end
+                                   
+                    @course_themes.each do |theme|
+                         if params[theme.theme_id.to_s] == 'chosen' and question.course_themes.include? theme
+                              @generated_exam.questions << question
+                              break
+                         end
+                    end
+                              
+                    # Themeless questions are handled separately because they have different params id
+                    if params.include? 'themeless' and question.course_theme_questions.empty?
+                         @generated_exam.questions << question
+                    end
+                              
+               end
+                         
+               # Fix the question numbers
+               @generated_exam.questions.each_with_index do |question, index|
+                    question.number = index+1
+                    @generated_exam.maximum_points = @generated_exam.maximum_points + question.points
+               end
+
+               # Make the hidden id string passing the question ids
+               @questions_string = ''
+               
+               @generated_exam.questions.each do |question|
+                    if question.number == 1
+                         @questions_string = @questions_string + question.id.to_s
+                    else
+                         @questions_string = @questions_string + ',' + question.id.to_s
+                    end
+               end
                
                          
           elsif params[:commit] == "PDF"
@@ -147,6 +209,7 @@ class ExamsController < ApplicationController
                @generated_exam.course_id = @course.id
                @generated_exam.date = Date.today
                @generated_exam.type_id = Type.find_by_name_fi("Generoitu koe").id
+               @generated_exam.maximum_points = 0
           
                # Parse the question ids from params and add them to the exam
                @question_ids = params['questions'].split(',')
@@ -158,6 +221,7 @@ class ExamsController < ApplicationController
                # Fix the question numbers
                @generated_exam.questions.each_with_index do |question, index|
                     question.number = index+1
+                    @generated_exam.maximum_points = @generated_exam.maximum_points + question.points
                end
                
                send_data PdfExport.exam(@generated_exam), :filename => "#{@generated_exam.course.name} - #{@generated_exam.name}.pdf"
