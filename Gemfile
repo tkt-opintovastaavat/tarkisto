109d52ecfa9d7d324c7db43db55c669c13ddf26a
Sami Saada <saada@cs.helsinki.fi>
2011-09-06 21:34:37 +0300

    added bundler

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+source 'http://gemcutter.org'
+
+gem 'rails', '2.3.5'
+
+gem 'sqlite3'
+
+gem 'haml'
+gem 'compass'
+gem 'net-ldap'
+gem 'pdf-writer'
+gem 'tabs_on_rails', '1.3.3'
+gem 'paperclip'
--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,13 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'ostruct'
 
 Rails::Initializer.run do |config|
-
-     config.gem "haml"
-     config.gem "compass", :version => ">=0.10.1"
-     config.gem "net-ldap", :lib => 'net/ldap'
-     config.gem "pdf-writer", :lib => 'pdf/writer', :version => ">=1.1.0"
-     config.gem "tabs_on_rails", :source => "http://gemcutter.org" 
-     config.gem "paperclip"
 
      config.time_zone = 'UTC'
      config.action_controller.allow_forgery_protection = false
