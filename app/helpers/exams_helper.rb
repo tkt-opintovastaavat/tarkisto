738f4e1ebea3ee430c061e8e7f69eddd1b820f59
Sami Saada <saada@cs.helsinki.fi>
2011-09-17 13:33:22 +0300

    cleaned helpers

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -1,52 +1,52 @@
 module ExamsHelper
-     include DataObject
+  include DataObject
 
-     def question_format question
-          description = ""
+  def question_format question
+    description = ""
 
-          attachments = question.images + question.code_snippets
+    attachments = question.images + question.code_snippets
 
 
-          filter = /<\[[ci]#[0-9]+\]>/
-          attach = /<\[([ci])#([0-9]+)\]>/
-          texts = question.description.split(filter)
-          objects = question.description.scan(filter).map do |object|
-               object = object.split(attach)
-               if object[1] == 'i'
-                    question.images[object[2].to_i-1]
-               else
-                    question.code_snippets[object[2].to_i-1]
-               end
-          end
+    filter = /<\[[ci]#[0-9]+\]>/
+    attach = /<\[([ci])#([0-9]+)\]>/
+    texts = question.description.split(filter)
+    objects = question.description.scan(filter).map do |object|
+      object = object.split(attach)
+      if object[1] == 'i'
+        question.images[object[2].to_i-1]
+      else
+        question.code_snippets[object[2].to_i-1]
+      end
+    end
 
-          if texts.empty?
-               texts << ""
-          end
-          texts.each do |str|
-               description = description + str
-               unless objects.empty?
-                    object = objects.shift
-                    if object.instance_of? Image
-                         description = description + "<img src='#{object.question_image.url(:web)}' />"
-                         attachments.delete object
-                    else
-                         description = description + "#{object.text}"
-                         attachments.delete object
-                    end
-               end
-          end
-          
-          attachments.each do |a|
-               objects << a
-          end
+    if texts.empty?
+      texts << ""
+    end
+    texts.each do |str|
+      description = description + str
+      unless objects.empty?
+        object = objects.shift
+        if object.instance_of? Image
+          description = description + "<img src='#{object.question_image.url(:web)}' />"
+          attachments.delete object
+        else
+          description = description + "#{object.text}"
+          attachments.delete object
+        end
+      end
+    end
 
-          objects.each do |object|
-               if object.instance_of? Image
-                    description = description + "<img src='#{object.question_image.url(:web)}' /> <br />"
-               else
-                    description = description + "#{object.text} <br />"
-               end
-          end
-          return description
-     end
+    attachments.each do |a|
+      objects << a
+    end
+
+    objects.each do |object|
+      if object.instance_of? Image
+        description = description + "<img src='#{object.question_image.url(:web)}' /> <br />"
+      else
+        description = description + "#{object.text} <br />"
+      end
+    end
+    return description
+  end
 end
