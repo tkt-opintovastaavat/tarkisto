79a78fff57d554dbd0397e17c78b1521b8e54499
Sami Saada <saada@cs.helsinki.fi>
2011-09-18 13:08:38 +0300

    added methods to get used levels

--- app/models/level.rb
+++ app/models/level.rb
@@ -3,6 +3,22 @@ class Level < ActiveRecord::Base
 
   validates_presence_of :name_fi
 
+  def self.basic_studies
+    self.find_by_name_fi("Perusopinnot")
+  end
+
+  def self.intermediate_studies
+    self.find_by_name_fi("Aineopinnot")
+  end
+
+  def self.advanced_studies
+    self.find_by_name_fi("Syventävät opinnot")
+  end
+
+  def self.other_studies
+    self.find_by_name_fi("Muut opinnot")
+  end
+
   def name
     if (I18n.locale == :fi)
       return name_fi
--- spec/models/level_spec.rb
+++ spec/models/level_spec.rb
@@ -15,4 +15,25 @@ describe Level do
   it "should have a name" do
     @invalid_level.should have(1).error_on(:name_fi)
   end
+
+  describe "names" do
+
+    it "should have basic studies" do
+      Level.basic_studies.name.should == "Perusopinnot"
+    end
+
+    it "should have intermediate studies" do
+      Level.intermediate_studies.name.should == "Aineopinnot"
+    end
+
+    it "should have advanced studies" do
+      Level.advanced_studies.name.should == "Syventävät opinnot"
+    end
+
+    it "should have other studies" do
+      Level.other_studies.name.should == "Muut opinnot"
+    end
+
+  end
+
 end
