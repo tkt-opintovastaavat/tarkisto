a82859113071d5f18e5a1ea83a9c2c52eebb68ae
Sami Saada <sami.saada@gmail.com>
2011-10-02 12:10:56 +0300

    development server works withous ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,38 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.root :controller => :dashboard, :action => :show, :ssl => true
-  map.resource :dashboard, :controller => :dashboard, :only => [:show], :ssl => true
+  ssl = !Rails.env.development?
 
-  map.resource :session, :controller => :session, :only => [:create, :destroy], :ssl => true
+  map.root :controller => :dashboard, :action => :show, :ssl => ssl
+  map.resource :dashboard, :controller => :dashboard, :only => [:show], :ssl => ssl
 
-  map.resource :search, :controller => :search, :only => [:show], :member => {:courses => :get}, :ssl => true
+  map.resource :session, :controller => :session, :only => [:create, :destroy], :ssl => ssl
 
-  map.resource :faq, :controller => :faq, :only => [:show], :ssl => true
+  map.resource :search, :controller => :search, :only => [:show], :member => {:courses => :get}, :ssl => ssl
 
-  map.resource :image, :only => [:create], :member => {:formula => :post}, :ssl => true
+  map.resource :faq, :controller => :faq, :only => [:show], :ssl => ssl
 
-  #map.connect 'image/:id', :controller => 'images', :action => 'getImageURL', :id => '/\d+, :ssl => true'
+  map.resource :image, :only => [:create], :member => {:formula => :post}, :ssl => ssl
 
-  map.resources :codes, :only => [:create, :show], :ssl => true
+  #map.connect 'image/:id', :controller => 'images', :action => 'getImageURL', :id => '/\d+, :ssl => ssl'
 
-  map.resources :courses, :collection => {:basics => :get, :intermediates => :get, :advanceds => :get, :others => :get}, :only => [:index], :ssl => true do |course|
-    course.resources :exams, :only => [:index, :show, :new, :create], :collection => {:generate => :get, :generate_preview => :get, :preview => :post, :publish => :post}, :ssl => true
+  map.resources :codes, :only => [:create, :show], :ssl => ssl
+
+  map.resources :courses, :collection => {:basics => :get, :intermediates => :get, :advanceds => :get, :others => :get}, :only => [:index], :ssl => ssl do |course|
+    course.resources :exams, :only => [:index, :show, :new, :create], :collection => {:generate => :get, :generate_preview => :get, :preview => :post, :publish => :post}, :ssl => ssl
   end
 
-  map.admin 'admin', :controller => 'admin/admin', :ssl => true
+  map.admin 'admin', :controller => 'admin/admin', :ssl => ssl
 
   map.namespace :admin do |admin|
-    admin.resources :courses, :active_scaffold => true, :ssl => true
-    admin.resources :users, :active_scaffold => true, :ssl => true
-    admin.resources :exams, :active_scaffold => true, :ssl => true
-    admin.resources :themes, :active_scaffold => true, :ssl => true
-    admin.resources :instances, :active_scaffold => true, :ssl => true
-    admin.resources :shortcuts, :active_scaffold => true, :ssl => true
-    admin.resources :levels, :active_scaffold => true, :ssl => true
-    admin.resources :course_themes, :active_scaffold => true, :ssl => true
-    admin.resources :types, :active_scaffold => true, :ssl => true
+    admin.resources :courses, :active_scaffold => true, :ssl => ssl
+    admin.resources :users, :active_scaffold => true, :ssl => ssl
+    admin.resources :exams, :active_scaffold => true, :ssl => ssl
+    admin.resources :themes, :active_scaffold => true, :ssl => ssl
+    admin.resources :instances, :active_scaffold => true, :ssl => ssl
+    admin.resources :shortcuts, :active_scaffold => true, :ssl => ssl
+    admin.resources :levels, :active_scaffold => true, :ssl => ssl
+    admin.resources :course_themes, :active_scaffold => true, :ssl => ssl
+    admin.resources :types, :active_scaffold => true, :ssl => ssl
   end
 
 end
