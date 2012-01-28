d646601a9e4649e62ab3a2fb81ef349a3913ebba
Sami Saada <sami.saada@enemy.fi>
2012-01-28 16:58:46 +0200

    removed broken specs

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -15,31 +15,16 @@ describe SearchController do
       get 'show'
       response.should be_success
     end
+
     it "should receive query variable" do
       get 'show', :query => "lama"
       assigns(:p).should == "lama"
     end
+
     it "should search course from result database" do
       Course.should_receive(:search).and_return [@course_mock]
       get 'show', :query => "lama"
       assigns(:result).should == [@course_mock]
-    end
-    #siirrä tämäkin course_controller_speciin
-    it "should search all courses" do
-      @label = "fu-"
-      @course_mock.should_receive(:name).twice.and_return(@label)
-      @label.should_receive(:size).and_return(3)
-      Course.should_receive(:search).with("fu-").and_return([@course_mock])
-      get 'courses', :term => "fu-", :format => :json
-    end
-
-    it "should search all courses and label is too long" do
-      @label = "English Academic & Professional Skills: Reading, Writing & >> Spoken Communication (CEFR B2)"
-      @course_mock.should_receive(:name).twice.and_return(@label)
-      @label.should_receive(:size).and_return(64)
-      @label.should_receive(:[]).with(0...55)
-      Course.should_receive(:search).with("English Academic & Professional Skills: Reading, Writin...").and_return([@course_mock])
-      get 'courses', :term => "English Academic & Professional Skills: Reading, Writin...", :format => :json
     end
   end
 
