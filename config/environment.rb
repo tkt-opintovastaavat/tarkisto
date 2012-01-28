780f556d14805b61789cbde448edf08778deb388
Sami Saada <sami.saada@enemy.fi>
2012-01-28 13:49:25 +0200

    updated rails 2.3.14 and removed some deprecation warnings

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,11 @@
-# Be sure to restart your server when you modify this file
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 require 'ostruct'
 
 Rails::Initializer.run do |config|
 
-     config.time_zone = 'UTC'
-     config.action_controller.allow_forgery_protection = false
-     config.i18n.default_locale = :fi
+  config.time_zone = 'UTC'
+  config.action_controller.allow_forgery_protection = false
+  config.i18n.default_locale = :fi
 
 end
