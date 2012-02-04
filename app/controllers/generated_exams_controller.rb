2b29e7092215a02173e14164684161e485a45c94
Sami Saada <sami.saada@enemy.fi>
2012-02-05 01:07:48 +0200

    removed repetation

--- app/controllers/generated_exams_controller.rb
+++ app/controllers/generated_exams_controller.rb
@@ -8,7 +8,6 @@ class GeneratedExamsController < ApplicationController
   def new
     respond_to do |format|
       format.html do
-        @course = Course.find_by_id params[:course_id]
         @exams = @course.exams.published
         @exams = @exams.only_public unless signed_in?
         @themes = @course.themes
