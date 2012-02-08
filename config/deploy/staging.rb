3efd343b6c61c4f2e4110f3695f66158e758a9e3
Sami Saada <sami.saada@enemy.fi>
2012-02-08 23:55:53 +0200

    separated staging from master

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :rvm_ruby_string, '1.8.7@tarkisto-production'
 set :deploy_to, "/home/#{user}/test"
 set :rails_env, "production"
-set :branch, "master"
+set :branch, "test"
 
 set :app_name, 'test'
 
