47f970eb5092b206aa2209ab701c1c5dc3d1ed20
Sami Saada <sami.saada@gmail.com>
2012-07-24 23:22:49 +0300

    number comes automatically

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+
   belongs_to :exam
 
   has_many :course_theme_questions
@@ -11,7 +12,14 @@ class Question < ActiveRecord::Base
 
   validates_presence_of :number, :name, :points
 
+  before_validation_on_create :assign_number
+
+  def assign_number
+    self.number ||= self.exam.questions.count + 1
+  end
+
   def themes
     course_themes.map { |course_theme| course_theme.theme }
   end
+
 end
